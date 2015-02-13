848043efc95492a31686767c7e5a04d151a22245
bportnoy <bradley.portnoy@gmail.com>
2015-02-12 16:24:31 -0800

    "fuck this shit"

--- src/solvers.js
+++ src/solvers.js
@@ -15,53 +15,13 @@
 
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
-  var board = new Board(n);
-
   //generate board with n spaces
-  //place n rooks on it
-  //test for rooks solution
-  //if a solution, assign to solution
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
-window.generateBoards = function(n){
-  var boards = [];
-  board = new Board({'n': n});
 
-  var generate = function(board,piecesPlaced){
-    var piecesPlaced = piecesPlaced || 0;
-    if ( piecesPlaced === n ){
-      console.log('pushed\n' + JSON.stringify(board.rows()));
-      //slice this or something
-      boards.push(board);
-      return;
-    }
-    //otherwise, toggle a bit
-
-    // iterate over rows
-    for( var x = piecesPlaced; x < n; x++ ) {
-
-      // iterate over columns
-      for ( var y = piecesPlaced; y < n; y ++ ){
-        // console.log(x + ',' + y);
-        board.togglePiece(x,y);
-        // console.log('flipped a piece at '+ x + ',' + y + '\n' + JSON.stringify(board.rows()));
-        piecesPlaced++;
-        generate(board, piecesPlaced);
-        board.togglePiece(x,y);
-        piecesPlaced--;
-        // console.log('flipped a piece at '+ x + ',' + y + '\n' + JSON.stringify(board.rows()));
-      }
-
-    }
-  };
-
-  generate(board);
-  console.log(JSON.stringify(boards));
-  return boards;
-};
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
